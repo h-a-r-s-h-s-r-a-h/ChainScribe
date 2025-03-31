37e8ef41e1ae73c3e6afd63439b093dfc5d52e92
Harsh <harshvirat894@gmail.com>
2025-04-01 00:12:12 +0530

    chainscribe

--- anchor-chainscribe-program/programs/anchor-chainscribe-program/src/errors.rs
+++ anchor-chainscribe-program/programs/anchor-chainscribe-program/src/errors.rs
@@ -2,19 +2,13 @@ use anchor_lang::prelude::*;
 
 #[error_code]
 pub enum TopicAccountError {
-    #[msg("Topic generator name is too large.")]
-    GeneratorNameTooLarge,
-    #[msg("")]
-
+    #[msg("Topic generator name too long!")]
+    GeneratorNameTooLong,
+    #[msg("Topic id too long!")]
+    TopicIdTooLong,
+    #[msg("Topic title too long!")]
+    TopicTitleTooLong,
+    #[msg("Topic Description too long!")]
+    TopicDescriptionTooLong,
 }
 
-
-//     pub topic_id: String,
-//     pub topic_title: String,
-//     pub topic_description: String,
-//     pub no_of_blog: u32,
-//     pub likes: u32,
-//     pub comments: u32,
-//     pub is_active: bool,
-//     pub is_public: bool,
-//     pub last_updated_at: i64,
